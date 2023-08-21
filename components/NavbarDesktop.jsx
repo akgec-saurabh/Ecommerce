130ee22627e3dd75d88cf8dbf34af11d7f9ed168
Saurabh Kumar <akgec.saurabh@gmail.com>
2023-08-21 21:53:11 +0530

    Desktop Nav

---
+++ components/NavbarDesktop.jsx
@@ -0,0 +1,75 @@
+import Link from "next/link";
+import React from "react";
+import { LiaHeart, LiaHeartSolid } from "react-icons/lia";
+import { AiOutlineShopping } from "react-icons/ai";
+import menu from "@/helpers/menu";
+
+const DropDown = ({ head, items }) => {
+  return (
+    <li className="relative h-full group">
+      <div className="h-full flex items-center px-2 py-2 cursor-pointer text-gray-600 hover:text-gray-950 ">
+        {head}
+      </div>
+      <ul className="absolute top-[100%] hidden py-4 px-6 group-hover:block bg-gray-950">
+        {items.map((item) => (
+          <li key={item.value} className="w-max">
+            <Link
+              href={item.value}
+              className="w-max cursor-pointer text-gray-400 hover:text-white text-sm"
+            >
+              {item.text}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </li>
+  );
+};
+
+const LeftMenu = () => {
+  return (
+    <ul className="flex items-center gap-2">
+      {menu.map((menu) => (
+        <DropDown key={menu.text} head={menu.text} items={menu.subMenu} />
+      ))}
+    </ul>
+  );
+};
+
+const Logo = () => {
+  return (
+    <div className="text-2xl font-medium uppercase flex items-center tracking-widest ">
+      FurniZen
+    </div>
+  );
+};
+
+const RightMenu = () => {
+  return (
+    <ul className="flex items-center gap-5 py-4">
+      <li className="text-gray-600 hover:text-gray-950 cursor-pointer">
+        <LiaHeart />
+      </li>
+      <li className=" text-gray-600 hover:text-gray-950 cursor-pointer">
+        Login
+      </li>
+      <li className=" text-gray-600 hover:text-gray-950 cursor-pointer">
+        <AiOutlineShopping />
+      </li>
+    </ul>
+  );
+};
+
+function NavbarDesktop() {
+  return (
+    <div className=" w-screen justify-betweencreen shadow	h-[70px]">
+      <div className="flex justify-between max-w-7xl mx-auto h-full">
+        <LeftMenu />
+        <Logo />
+        <RightMenu />
+      </div>
+    </div>
+  );
+}
+
+export default NavbarDesktop;
---
+++ helpers/menu.js
@@ -0,0 +1,80 @@
+const menu = [
+  {
+    text: "Shop",
+    subMenu: [
+      {
+        text: "Cart",
+        value: "cart",
+      },
+      {
+        text: "Checkout",
+        value: "checkout",
+      },
+      {
+        text: "Order Tracking",
+        value: "order-tracking",
+      },
+      {
+        text: "My Account",
+        value: "my-account",
+      },
+      {
+        text: "Wishlist",
+        value: "wishlist",
+      },
+    ],
+  },
+  {
+    text: "Category",
+    subMenu: [
+      {
+        text: "All",
+        value: "",
+      },
+      {
+        text: "Bags & Backpacks",
+        value: "bags-backpacks",
+      },
+      {
+        text: "Decoration",
+        value: "decoration",
+      },
+      {
+        text: "Essentials",
+        value: "essentials",
+      },
+      {
+        text: "Interior",
+        value: "interior",
+      },
+    ],
+  },
+  {
+    text: "Pages",
+    subMenu: [
+      {
+        text: "Portfolio",
+        value: "portfolio",
+      },
+      {
+        text: "About Us",
+        value: "about-us",
+      },
+      {
+        text: "Contact",
+        value: "contact",
+      },
+
+      {
+        text: "FAQ",
+        value: "faq",
+      },
+      {
+        text: "404",
+        value: "404",
+      },
+    ],
+  },
+];
+
+export default menu;
